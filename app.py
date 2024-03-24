ac97b7507847a00b0f83df7456a085f83ebed14c
nicoig <n.guerravillalobos@gmail.com>
2024-03-24 18:14:47 -0300

    Se actualizan las variables de entorno

--- app.py
+++ app.py
@@ -4,14 +4,21 @@
 # Primera Carga a Github
 # git init
 # git add .
-# git remote add origin https://github.com/nicoig/GPT-Clone.git
+# git remote add origin https://github.com/nicoig/ClonGPT.git
 # git commit -m "Initial commit"
 # git push -u origin master
+
 
 # Actualizar Repo de Github
 # git add .
 # git commit -m "Se actualizan las variables de entorno"
 # git push origin master
+
+'''
+git add .
+git commit -m "Se actualizan las variables de entorno"
+git push origin master
+'''
 
 # En Render
 # agregar en variables de entorno
@@ -22,99 +29,79 @@
 
 import streamlit as st
 import time
-from openai import OpenAI
 import os
 from dotenv import load_dotenv
-from openai.file_upload import FileUpload
+import openai
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter
 
-# Carga las variables de entorno desde el archivo .env
+st.set_page_config(
+    page_title="ClonGPT",
+    layout="centered",
+    initial_sidebar_state="collapsed",
+    menu_items={
+        'Get Help': None,
+        'Report a bug': None,
+        'About': None
+    }
+)
+
 load_dotenv()
-api_key = os.getenv("OPENAI_API_KEY")
+api_key = os.getenv("OPENAI_APIKEY")
 assistant_id = os.getenv("ASSISTANT_ID")
 
-# Set openAi client, assistant ai and assistant ai thread
 @st.cache_resource
 def load_openai_client_and_assistant():
-    client = OpenAI(api_key=api_key)
+    client = openai.OpenAI(api_key=api_key)
     my_assistant = client.beta.assistants.retrieve(assistant_id)
     thread = client.beta.threads.create()
     return client, my_assistant, thread
 
 client, my_assistant, assistant_thread = load_openai_client_and_assistant()
 
-# Check in loop if assistant ai parse our request
 def wait_on_run(run, thread):
     while run.status == "queued" or run.status == "in_progress":
         run = client.beta.threads.runs.retrieve(thread_id=thread.id, run_id=run.id)
         time.sleep(0.5)
     return run
 
-# Initiate assistant ai response
-def get_assistant_response(user_input="", file_id=None):
+def get_assistant_response(user_input=""):
     with st.spinner('Escribiendo Respuesta...'):
-        message = client.beta.threads.messages.create(thread_id=assistant_thread.id, role="user", content=user_input, file_ids=[file_id] if file_id else [])
+        message = client.beta.threads.messages.create(thread_id=assistant_thread.id, role="user", content=user_input)
         run = client.beta.threads.runs.create(thread_id=assistant_thread.id, assistant_id=assistant_id)
         run = wait_on_run(run, assistant_thread)
         messages = client.beta.threads.messages.list(thread_id=assistant_thread.id, order="asc", after=message.id)
         return messages.data[0].content[0].text.value
 
-# Function to upload a file and get its ID
-def upload_file(file_path):
-    openai = OpenAI(api_key=api_key)
-    file_upload = FileUpload(openai, file_path, purpose="assistants")
-    file_id = file_upload.upload()
-    return file_id
+def save_conversation_to_pdf(conversation):
+    c = canvas.Canvas("conversation.pdf", pagesize=letter)
+    width, height = letter
+    c.setFont("Helvetica", 12)
+    for line in conversation:
+        c.drawString(10, height - 10, line)
+        height -= 15
+    c.save()
 
-# Streamlit app title
 st.markdown("<h1 style='text-align: center; color: white; font-size: 24px;'>ClonGPT</h1>", unsafe_allow_html=True)
-
-# Utiliza 7 columnas para un centrado más preciso
 col1, col2, col3, col4, col5, col6, col7, col8, col9 = st.columns([1,1,1,1,2,1,1,1,1])
 
-with col5: # Esta es la columna central
-    st.image("img/img.jpg", width=240) # Ajusta el ancho de la imagen según sea necesario
+with col5:
+    st.image("img/img.jpg", width=240)
 
-# Agregar la descripción debajo de la imagen
-st.markdown("""
-<p style="text-align: center;">
-Hola, soy ClonGPT, un asistente virtual creado para responder sobre cualquier tema que desees. Puedes cargarme archivos y realizar consultas sobre ellos, o simplemente hacer preguntas sobre cualquier asunto que te interese. Estoy aquí para ayudarte de la mejor manera posible.
-</p>
-""", unsafe_allow_html=True)
+st.markdown(""" <p style="text-align: center;"> Hola, soy ClonGPT, un asistente virtual creado para responder sobre cualquier tema que desees. Puedes cargarme archivos y realizar consultas sobre ellos, o simplemente hacer preguntas sobre cualquier asunto que te interese. Estoy aquí para ayudarte de la mejor manera posible. </p> """, unsafe_allow_html=True)
 
-# Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
 
-# Display chat messages from history
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
         st.markdown(message["content"])
 
-# Accept user input
 if prompt := st.chat_input("Escribe tu consulta..."):
-    # Add user message to chat history and display
     st.session_state.messages.append({"role": "user", "content": prompt})
     with st.chat_message("user"):
         st.markdown(prompt)
-
-    # Allow user to upload a file
-    file_upload = st.file_uploader("Carga un archivo", type=["pdf", "txt", "docx"])
-    if file_upload is not None:
-        file_path = f"temp/{file_upload.name}"
-        with open(file_path, "wb") as f:
-            f.write(file_upload.getbuffer())
-        file_id = upload_file(file_path)
-        os.remove(file_path)
-
-        # Get assistant response with file
-        response = get_assistant_response(prompt, file_id=file_id)
-    else:
-        # Get assistant response without file
-        response = get_assistant_response(prompt)
-
-    # Display assistant response in chat message container
+    response = get_assistant_response(prompt)
     with st.chat_message("assistant"):
         st.markdown(response)
-
-    # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
--- readme.md
+++ readme.md
@@ -1,58 +1,42 @@
-# Jacobo Grinberg IA
+# ClonGPT
 
-Este proyecto es una aplicación interactiva de Streamlit que simula conversaciones con Jacobo Grinberg, ofreciendo respuestas generadas por IA a preguntas sobre psicología, espiritualidad y las investigaciones de Grinberg. Utiliza la API de OpenAI para generar las respuestas y la API de ElevenLabs para convertir el texto de las respuestas en audio.
+ClonGPT es una aplicación de asistente virtual creada con Streamlit y OpenAI. Esta aplicación te permite tener una conversación interactiva con un asistente virtual, similar a ChatGPT, pero personalizado con tus propias preferencias.
 
-## Características
+## Características principales
 
-- Interfaz de chat para interactuar con la IA que simula a Jacobo Grinberg.
-- Respuestas en texto generadas por la API de OpenAI.
-- Conversión de respuestas de texto a audio mediante la API de ElevenLabs.
+- **Interfaz de chat intuitiva**: La aplicación presenta una interfaz de chat limpia y fácil de usar, donde puedes escribir tus consultas y recibir respuestas del asistente virtual.
+- **Integración con OpenAI**: ClonGPT utiliza la API de OpenAI para generar respuestas inteligentes y relevantes a tus preguntas.
+- **Personalización del asistente**: Puedes configurar y personalizar el comportamiento del asistente virtual, ajustando sus parámetros y preferencias según tus necesidades.
+- **Generación de PDF**: La aplicación te permite guardar las conversaciones completas en un archivo PDF para futuras referencias.
 
-## Cómo usar
+## Requisitos
 
-Para utilizar la aplicación, simplemente ingresa tu consulta en el campo de texto disponible y la IA responderá. También puedes escuchar la respuesta en formato de audio.
+- Python 3.x
+- Streamlit
+- OpenAI
+- ReportLab
 
 ## Instalación
 
-Para ejecutar esta aplicación en tu entorno local, sigue estos pasos:
+1. Clona este repositorio en tu máquina local.
+2. Instala las dependencias necesarias ejecutando `pip install -r requirements.txt`.
+3. Configura tus credenciales de OpenAI creando un archivo `.env` en la raíz del proyecto y agregando tus claves de API de OpenAI:
 
-1. Clona este repositorio:
+OPENAI_APIKEY=tu_clave_api
+ASSISTANT_ID=id_de_tu_asistente
 
-git clone https://github.com/nicoig/jacobo-grinberg-ia
+## Uso
 
+1. Ejecuta la aplicación con `streamlit run app.py`.
+2. La aplicación se abrirá en tu navegador web predeterminado.
+3. Escribe tus consultas en el campo de entrada y el asistente virtual generará respuestas relevantes.
+4. Puedes guardar la conversación completa en un archivo PDF haciendo clic en el botón correspondiente.
 
-2. Instala las dependencias:
+## Contribuciones
 
-pip install -r requirements.txt
-
-
-3. Ejecuta la aplicación de Streamlit:
-
-streamlit run app_audio.py
-
-
-## Configuración
-
-Para configurar las claves API necesarias para OpenAI y ElevenLabs, sigue estos pasos:
-
-1. Crea un archivo `.env` en el directorio raíz del proyecto con el siguiente contenido, reemplazando `<tu-clave-api>` con tus respectivas claves API:
-
-OPENAI_APIKEY=<tu-clave-api-de-openai>
-ASSISTANT_ID=<tu-id-de-asistente-de-openai>
-XI_API_KEY=<tu-clave-api-de-elevenlabs>
-
-
-2. Asegúrate de que el archivo `.env` esté listado en tu `.gitignore` para prevenir que se suba a repositorios públicos.
-
-## Contribuir
-
-Si te gustaría contribuir a este proyecto, por favor, crea un fork del repositorio, realiza tus cambios y envía un pull request para su revisión.
+¡Las contribuciones son bienvenidas! Si deseas mejorar esta aplicación, por favor, crea un issue o envía una solicitud de extracción.
 
 ## Licencia
 
-Este proyecto está bajo la Licencia MIT. Consulta el archivo `LICENSE` para más detalles.
+Este proyecto está bajo la Licencia MIT. 
 
-## Contacto
-
-Si tienes preguntas o comentarios sobre este proyecto, por favor, no dudes en contactarme.
-Recuerda reemplazar <URL-del-repositorio>, <tu-clave-api-de-openai>, <tu-id-de-asistente-de-openai>, y <tu-clave-api-de-elevenlabs> con la información correspondiente a tu proyecto. Asegúrate también de ajustar cualquier otra información específica de tu proyecto como la descripción, cómo usar, configuración y contacto.
